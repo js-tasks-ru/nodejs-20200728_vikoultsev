36c4539af5ffe1948ee4d24c3ea4bcab3c6a07a4
Oleg Vikoultsev <vikoultsev@gmail.com>
2020-08-13 23:09:13 +0800

    add solution 05-http-server-streams 01-file-server-get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,21 +1,41 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
+  const parsedURL = path.parse(req.url);
+
+  if (parsedURL.root !== parsedURL.dir) {
+    res.statusCode = 400;
+    res.end('Invalid URL!');
+    return;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      fs.access(filepath, fs.constants.R_OK, (err) => {
+        if (!err) {
+          fs.createReadStream(filepath)
+              .on('error', (err) => {
+                console.log(err);
+              })
+              .pipe(res);
+        } else {
+          res.statusCode = 404;
+          res.end('Not found!');
+        }
+      });
 
       break;
 
     default:
-      res.statusCode = 501;
+      res.statusCode = 500;
       res.end('Not implemented');
   }
 });
