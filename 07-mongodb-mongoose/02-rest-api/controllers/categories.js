a124479b591a70643c46d5c2959f1c0a3267dfbf
Oleg Vikoultsev <vikoultsev@gmail.com>
2020-08-24 03:48:07 +0800

    add solution 07-mongodb-mongoose 02-rest-api

--- 07-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,15 @@
+const Category = require('../models/Category');
+
+const mapCategory = (category) => ({
+  id: category._id,
+  title: category.title,
+  subcategories: category.subcategories.map((subCategory) => ({
+    id: subCategory._id,
+    title: subCategory.title,
+  })),
+});
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find({}).populate('subcategories');
+  ctx.body = {categories: categories.map(mapCategory)};
 };
--- 07-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 07-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,5 +1,27 @@
+const _ = require('lodash');
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+
+const validateObjectId = (objectId) => mongoose.Types.ObjectId.isValid(objectId);
+
+const pickProductData = (productFromDB) => _.pick(productFromDB, [
+  'title',
+  'id',
+  'category',
+  'subcategory',
+  'price',
+  'description',
+  'images',
+]);
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
-  ctx.body = {};
+  const subcategory = ctx.request.query.subcategory;
+  const params = subcategory ? {subcategory} : {};
+  const products = await Product.find(params);
+
+  ctx.body = {products: products.map((product) => {
+    return pickProductData(product);
+  })};
 };
 
 module.exports.productList = async function productList(ctx, next) {
@@ -7,6 +29,15 @@ module.exports.productList = async function productList(ctx, next) {
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  if (ctx.params.id && validateObjectId(ctx.params.id)) {
+    const product = await Product.findById(ctx.params.id);
+    if (product) {
+      ctx.body = {product: pickProductData(product)};
+    } else {
+      ctx.status = 404;
+    }
+  } else {
+    ctx.status = 400;
+  }
 };
 
